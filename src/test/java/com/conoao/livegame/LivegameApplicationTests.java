370343b2f5d207c40bb2582547e02a98c68574a9
chenhaidong <314212212@qq.com>
2018-03-21 16:26:57 +0800

    初始化

---
+++ src/test/java/com/conoao/livegame/LivegameApplicationTests.java
@@ -0,0 +1,16 @@
+package com.conoao.livegame;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LivegameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
